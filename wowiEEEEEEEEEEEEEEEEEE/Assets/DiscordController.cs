c9b1d2a60f59146d3a99fb37c7f26f1b1bc24686
gamerlol5 <gamerlol5epic@gmail.com>
2022-11-01 20:45:23 -0400

    haha game go bRRRRRRRRRRRRRRRRRRRRRRRRRRR

--- wowiEEEEEEEEEEEEEEEEEE/Assets/DiscordController.cs
+++ wowiEEEEEEEEEEEEEEEEEE/Assets/DiscordController.cs
@@ -13,7 +13,31 @@ public class DiscordController : MonoBehaviour
         var activityManager = discord.GetActivityManager();
         var activity = new Discord.Activity
         {
-
+            State = "Shooting circles to defend a circle",
+            Details = "haha gun go pew pew",
+            Timestamps =
+            {
+                Start = 1667344362
+            },
+            Assets =
+            {
+                LargeImage = "balanceiconthing"
+            }
         };
+        activityManager.UpdateActivity(activity, (res) =>
+        {
+            if (res == Result.Ok)
+            {
+                Debug.Log("Discord successfully connected!");
+            } else
+            {
+                Debug.LogError("Discord connection failed");
+            }
+        });
+    }
+
+    private void Update()
+    {
+        discord.RunCallbacks();
     }
 }
--- wowiEEEEEEEEEEEEEEEEEE/Assets/Scripts/Wall.cs
+++ wowiEEEEEEEEEEEEEEEEEE/Assets/Scripts/Wall.cs
@@ -7,12 +7,19 @@ public class Wall : MonoBehaviour
     public int hp;
 
     public ParticleSystem destroyEffect;
+    public AudioSource wallGoBoom;
+
+    private void Start()
+    {
+        wallGoBoom = GameObject.Find("buildGoBoom").GetComponent<AudioSource>(); 
+    }
 
     private void Update()
     {
         if (hp <= 0)
         {
             Instantiate(destroyEffect, transform.position, Quaternion.identity);
+            wallGoBoom.Play();
             Destroy(gameObject);
         }
     }
